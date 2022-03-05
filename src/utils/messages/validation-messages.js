459c18a368948e62b71d97ff16cb43cc70a7d8ec
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2022-03-05 15:34:51 -0300

    created data format validation

--- src/utils/messages/validation-messages.js
+++ src/utils/messages/validation-messages.js
@@ -1,12 +1,16 @@
 const STATUS_IS_REQUIRED = 'Status is required'
 const DATE_START_IS_REQUIRED = 'Date start is required'
 const DATE_END_IS_REQUIRED = 'Date end is required'
+const DATE_INVALID_FORMAT = 'Wrong date format'
+
 const VALIDATION_ERROR = 'Validation error'
+
 
 
 module.exports = {
     STATUS_IS_REQUIRED, 
     DATE_START_IS_REQUIRED,
     DATE_END_IS_REQUIRED,
-    VALIDATION_ERROR
+    VALIDATION_ERROR,
+    DATE_INVALID_FORMAT
 }
